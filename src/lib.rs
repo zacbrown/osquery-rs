08c19b5742c5c71fcf184db8d26acaa6726178b3
Zac Brown <zacbrown@users.noreply.github.com>
2018-06-09 16:57:03 -0700

    Implement our own version of TServer since the one provided by the thrift crate only supports a TCP server.
    Signed-off-by: Zac Brown <zacbrown@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 extern crate ordered_float;
 extern crate thrift;
+extern crate threadpool;
 extern crate try_from;
 extern crate uuid;
 
@@ -16,17 +17,35 @@ use std::sync::{
     MutexGuard
 };
 
+use threadpool::ThreadPool;
+
+use thrift::{
+    ApplicationError,
+    ApplicationErrorKind
+};
 use thrift::protocol::{
     TBinaryInputProtocol,
     TBinaryInputProtocolFactory,
     TBinaryOutputProtocol,
-    TBinaryOutputProtocolFactory,};
+    TBinaryOutputProtocolFactory,
+    TInputProtocolFactory,
+    TOutputProtocolFactory,
+    TInputProtocol,
+    TOutputProtocol,
+};
 use thrift::transport::{
     TBufferChannel,
     TBufferedReadTransport,
     TBufferedReadTransportFactory,
     TBufferedWriteTransport,
     TBufferedWriteTransportFactory,
+    TReadTransportFactory,
+    TWriteTransportFactory,
+    TIoChannel,
+};
+
+use thrift::server::{
+    TProcessor,
 };
 
 use osquery::*;
@@ -37,6 +56,7 @@ mod sys {
 
     pub const DEFAULT_PIPE_PATH: &'static str ="/var/osquery/osquery.em";
     pub type TChannel = unix_socket::UnixStream;
+    pub type TListener = unix_socket::UnixListener;
 }
 
 #[cfg(target_os = "windows")]
@@ -167,6 +187,133 @@ impl ExtensionSyncHandler for ExtensionClientHandler {
     }
 }
 
+#[derive(Debug)]
+pub struct TServer<PRC, RTF, IPF, WTF, OPF>
+    where
+        PRC: TProcessor + Send + Sync + 'static,
+        RTF: TReadTransportFactory + 'static,
+        IPF: TInputProtocolFactory + 'static,
+        WTF: TWriteTransportFactory + 'static,
+        OPF: TOutputProtocolFactory + 'static,
+{
+    r_trans_factory: RTF,
+    i_proto_factory: IPF,
+    w_trans_factory: WTF,
+    o_proto_factory: OPF,
+    processor: Arc<PRC>,
+    worker_pool: ThreadPool,
+}
+
+impl<PRC, RTF, IPF, WTF, OPF> TServer<PRC, RTF, IPF, WTF, OPF>
+    where PRC: TProcessor + Send + Sync + 'static,
+          RTF: TReadTransportFactory + 'static,
+          IPF: TInputProtocolFactory + 'static,
+          WTF: TWriteTransportFactory + 'static,
+          OPF: TOutputProtocolFactory + 'static {
+    /// Create a `TServer`.
+    ///
+    /// Each accepted connection has an input and output half, each of which
+    /// requires a `TTransport` and `TProtocol`. `TServer` uses
+    /// `read_transport_factory` and `input_protocol_factory` to create
+    /// implementations for the input, and `write_transport_factory` and
+    /// `output_protocol_factory` to create implementations for the output.
+    pub fn new(
+        read_transport_factory: RTF,
+        input_protocol_factory: IPF,
+        write_transport_factory: WTF,
+        output_protocol_factory: OPF,
+        processor: PRC,
+        num_workers: usize,
+    ) -> TServer<PRC, RTF, IPF, WTF, OPF> {
+        TServer {
+            r_trans_factory: read_transport_factory,
+            i_proto_factory: input_protocol_factory,
+            w_trans_factory: write_transport_factory,
+            o_proto_factory: output_protocol_factory,
+            processor: Arc::new(processor),
+            worker_pool: ThreadPool::with_name(
+                "Thrift service processor".to_owned(),
+                num_workers,
+            ),
+        }
+    }
+
+    fn bind(listen_address: &str) -> thrift::Result<sys::TListener> {
+        if cfg!(target_family = "unix") {
+            let socket = unix_socket::UnixListener::bind(listen_address)?;
+            Ok(socket)
+        } else {
+            unimplemented!();
+        }
+    }
+
+    pub fn listen(&mut self, listen_address: &str) -> thrift::Result<()> {
+        let listener = Self::bind(listen_address)?;
+        for stream in listener.incoming() {
+            match stream {
+                Ok(s) => {
+                    let (i_prot, o_prot) = self.new_protocols_for_connection(s)?;
+                    let processor = self.processor.clone();
+                    self.worker_pool
+                        .execute(move || handle_incoming_connection(processor, i_prot, o_prot),);
+                }
+                Err(e) => {
+                    println!("WARN: failed to accept remote connection with error {:?}", e);
+                }
+            }
+        }
+
+        Err(
+            thrift::Error::Application(
+                ApplicationError {
+                    kind: ApplicationErrorKind::Unknown,
+                    message: "aborted listen loop".into(),
+                },
+            ),
+        )
+    }
+
+
+    fn new_protocols_for_connection(
+        &mut self,
+        stream: sys::TChannel,
+    ) -> thrift::Result<(Box<TInputProtocol + Send>, Box<TOutputProtocol + Send>)> {
+        // split it into two - one to be owned by the
+        // input tran/proto and the other by the output
+        let w_chan = stream.try_clone()?;
+        let r_chan = stream;
+
+        // input protocol and transport
+        let r_tran = self.r_trans_factory.create(Box::new(r_chan));
+        let i_prot = self.i_proto_factory.create(r_tran);
+
+        // output protocol and transport
+        let w_tran = self.w_trans_factory.create(Box::new(w_chan));
+        let o_prot = self.o_proto_factory.create(w_tran);
+
+        Ok((i_prot, o_prot))
+    }
+}
+
+fn handle_incoming_connection<PRC>(
+    processor: Arc<PRC>,
+    i_prot: Box<TInputProtocol>,
+    o_prot: Box<TOutputProtocol>,
+) where
+    PRC: TProcessor,
+{
+    let mut i_prot = i_prot;
+    let mut o_prot = o_prot;
+    loop {
+        let r = processor.process(&mut *i_prot, &mut *o_prot);
+        if let Err(e) = r {
+            println!("WARN: processor completed with error: {:?}", e);
+            break;
+        }
+    }
+}
+
+
 pub struct ExtensionManagerServer {
     extension_name: String,
     listen_path: String,
@@ -257,7 +404,7 @@ impl ExtensionManagerServer {
         let out_protocol_factory = TBinaryOutputProtocolFactory::new();
         let in_transport_factory = TBufferedReadTransportFactory::new();
         let in_protocol_factory = TBinaryInputProtocolFactory::new();
-        let mut server = thrift::server::TServer::new(
+        let mut server = TServer::new(
             in_transport_factory,
             in_protocol_factory,
             out_transport_factory,
@@ -267,7 +414,21 @@ impl ExtensionManagerServer {
         );
 
         println!("listen path: {}", listen_path);
-        server.listen(&listen_path).unwrap();
+        const SLEEP_MS: u32 = 750;
+        for ii in 0..10 {
+            match server.listen(&listen_path) {
+                Ok(_) => break,
+                Err(e) => {
+                    if ii == 9 {
+                        println!("Unable to make a connection to the socket address :( -- {}", listen_path);
+                    } else {
+                        println!("Failed to connect to listen socket ({}), attempting again in {} millisecond.", listen_path, SLEEP_MS);
+                        std::thread::sleep_ms(SLEEP_MS);
+                    }
+                }
+            }
+        }
+        println!("beep beep");
         std::thread::sleep_ms(5000);
     }
 }
